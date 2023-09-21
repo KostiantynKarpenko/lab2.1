0ef303cc56a40a7643ce593664bfa37ba135033c
kosty <kostaframincuk89@gmail.com>
2023-09-22 02:04:10 +0300

    new file:   css/index.css 	new file:   img/svg/magnifying-glass-thin.svg 	new file:   img/svg/x-symbol.svg 	new file:   index.html 	new file:   js/index.js 	new file:   less/index.less

---
+++ js/index.js
@@ -0,0 +1,80 @@
+const API = 'https://pokeapi.co/api/v2/pokemon';
+let users = [];
+let filteredUsers = [];
+
+const $listContainer = document.querySelector('.users-table')
+const $searchBar = document.querySelector('.search-bar')
+const $field = document.querySelector('.field')
+const $cross = document.querySelector('.clear-search-bar')
+
+function setLS(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+function getLS(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+if (!!getLS('data')) {
+    users = getLS('data');
+    console.log(users)
+    templateBuilder(users);
+}
+else{
+    fetch(API)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        users = data.results;
+        setLS('data', users)
+        templateBuilder(users);
+    });
+}
+
+function filterController(query) {
+    filteredUsers = users.filter((el) => {
+        return ~el.name.toLowerCase().indexOf(query.toLowerCase());
+    });
+    templateBuilder(filteredUsers);
+}
+
+function templateBuilder(list) {
+    let template = '';
+    if (!list.length) {
+        template = '<tr><td><span>Not found</span></td></tr>'
+    }
+    else {
+        list.forEach(element => {
+            template += '<tr><td><a class="name">' + element.name + '</a></td>' + '<td><a class="url">' + element.url + '</a></td></tr>'
+        });
+    }
+    $listContainer.innerHTML = template;
+}
+
+$field.addEventListener('input', (e) => {
+    let query = e.target.value;
+    filterController(query);
+});
+
+window.addEventListener('click', (e) =>{
+    if (e.target.classList.contains('name')) {
+        let target = e.target.innerText
+        $field.value = target;
+        filterController(target)
+    }
+});
+
+$field.addEventListener('focus', () => {
+    $searchBar.classList.add('active');
+});
+
+$field.addEventListener('blur', () => {
+    $searchBar.classList.remove('active');
+});
+
+$cross.addEventListener('click', () => {
+    $field.value = "";
+    filterController("");
+});
+
+
